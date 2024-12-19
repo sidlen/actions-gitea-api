55826881a577597c8bbf019f05953e839e456a05
igoshindi <Dmitriy.Igoshin@softline.com>
2024-12-20 01:07:15 +0300

    add data

--- pr-info/pr.py
+++ pr-info/pr.py
@@ -10,18 +10,24 @@ commit_sha = os.getenv("GITEA_COMMIT_SHA")
 approvers = []
 approvers_emails = []
 
+def getUserEmail(user):
+  response = requests.get(f"{gitea_api_url}/users/{user}", headers=headers)
+  return response.json()["email"]
+
 response = requests.get(f"{gitea_api_url}/repos/{gitea_repository}/pulls", headers=headers)
 for pr in response.json():
     if pr["merge_commit_sha"] == commit_sha:
         pr_url = pr["html_url"]
+        pr_requster_email = getUserEmail(pr["user"]["login"])
+        pr_merge_user_email = getUserEmail(pr["merged_by"]["login"])
         for approver in pr["requested_reviewers"]:
-            approvers.append(approver["login"])
-for approver in approvers:
-    response = requests.get(f"{gitea_api_url}/users/{approver}", headers=headers)
-    approvers_emails.append(response.json()["email"])
+            approvers_emails.append(getUserEmail(approver["login"]))
+
 output_data = {
   "approvers_emails":approvers_emails,
-  "pr_url": pr_url
+  "pr_url": pr_url,
+  "pr_requster_email": pr_requster_email,
+  "pr_merge_user_email": pr_merge_user_email
 }
 with open('git_api.json', 'w', encoding='utf-8') as f:
     json.dump(output_data, f, indent=4, ensure_ascii=False)
